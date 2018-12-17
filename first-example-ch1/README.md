c7ac051f1c60bb984717337a19cd1cf7e7e850f4
Himanshu Ojha <himanshuo@gmail.com>
2018-12-17 16:37:02 -0500

    got code working

---
+++ first-example-ch1/README.md
@@ -0,0 +1,7 @@
+# First example 
+First example in Refactoring v2. 
+
+Reads invoices.json and plays.json in order to print out a formatted bill.
+
+# Run 
+> npm start
---
+++ first-example-ch1/src/index.js
@@ -0,0 +1,8 @@
+import { statement } from "./print_bill";
+const fs = require('fs');
+const invoices = JSON.parse(fs.readFileSync("./src/invoices.json"));
+const plays = JSON.parse(fs.readFileSync("./src/plays.json"));
+
+for (let invoice of invoices) {
+  console.log(statement(invoice, plays));
+}
