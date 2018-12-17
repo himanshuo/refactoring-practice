d5b9221179fb291bdcae451ccac9803eb688a611
Himanshu Ojha <himanshuo@gmail.com>
2018-12-17 18:27:54 -0500

    seperate totalAmount into its own function

--- first-example-ch1/src/print_bill.js
+++ first-example-ch1/src/print_bill.js
@@ -12,13 +12,44 @@ function usd(value) {
 }
 
 /**
+ * @param {Invoice} invoice
+ * @param {[Play]} plays list of plays.
+ * @return {Number} total amount to charge for the provided invoice
+ */
+function calculateTotalAmount(invoice, plays) {
+  let totalAmount = 0;
+  for (const perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy':
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    totalAmount += thisAmount;
+  }
+  return totalAmount;
+}
+
+/**
  * Provides properly-formatted bill provided an invoice and list of plays.
  * @param {Invoice} invoice invoice for a customer.
  * @param {[Play]} plays list of plays.
  * @return {String} properly-formatted bill.
  */
 export function statement(invoice, plays) {
-  let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (const perf of invoice.performances) {
@@ -51,9 +82,8 @@ export function statement(invoice, plays) {
     // print line for this order
     const amount = usd(thisAmount/100);
     result += `  ${play.name}: ${amount} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
   }
-  result += `Amount owed is ${usd(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(calculateTotalAmount(invoice, plays)/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
