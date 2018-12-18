340069f487d5962bf727f0cd74ccf93a5e88d198
Himanshu Ojha <himanshuo@gmail.com>
2018-12-18 10:44:55 -0500

    organize index and tst runner code. create tst resources

--- first-example-ch1/src/index.js
+++ first-example-ch1/src/index.js
@@ -1,9 +1,23 @@
+import {PlayManager} from './play/play_manager';
 import {Bill} from './bill';
+import {Performance} from './performance';
+import {Invoice} from './invoice';
 
 const fs = require('fs');
-const invoices = JSON.parse(fs.readFileSync('./src/invoices.json'));
-const plays = JSON.parse(fs.readFileSync('./src/plays.json'));
-const bill = new Bill(new PlayManager(plays));
+
+const playJson = JSON.parse(fs.readFileSync('./src/resources/plays.json'));
+const playManager = new PlayManager(playJson);
+const bill = new Bill(playManager);
+
+const invoicesJson = JSON.parse(
+    fs.readFileSync('./src/resources/invoices.json'));
+const invoices = invoicesJson.map((invoiceJson) => {
+  const performances = invoiceJson.performances.map((performanceJson) => {
+    const play = playManager.getPlay(performanceJson.playID);
+    return new Performance(play, performanceJson.audience);
+  });
+  return new Invoice(invoiceJson.customer, performances, playManager);
+});
 
 for (const invoice of invoices) {
   console.log(bill.statement(invoice));
